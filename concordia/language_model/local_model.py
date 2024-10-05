40f1f7ac9f4b2a762168d4bff61a2e79f9399002
Lancelot Liu <me@lancy.dev>
2024-10-05 13:46:34 +0100

    feat: Uses Llama.

--- concordia/language_model/local_model.py
+++ concordia/language_model/local_model.py
@@ -21,22 +21,15 @@ from collections.abc import Collection, Sequence
 import logging
 from textwrap import dedent
 
-from accelerate import Accelerator
 from concordia.language_model import language_model
 from concordia.utils import measurements as measurements_lib
-from mistral_common.protocol.instruct.messages import AssistantMessage
-from mistral_common.protocol.instruct.messages import SystemMessage
-from mistral_common.protocol.instruct.messages import UserMessage
-from mistral_common.protocol.instruct.request import ChatCompletionRequest
-from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
+from concordia.utils.sampling import extract_choice_response, dynamically_adjust_temperature
 import numpy as np
-import torch
-from transformers import AutoModelForCausalLM
-from transformers import MistralForCausalLM
+from llama_cpp import Llama
 from typing_extensions import override
 
 _DEFAULT_MAX_TOKENS = 5000
-
+_MAX_MULTIPLE_CHOICE_ATTEMPTS = 20
 
 class LocalModel(language_model.LanguageModel):
     """Language Model that runs Gemma-2 locally."""
@@ -54,46 +47,49 @@ class LocalModel(language_model.LanguageModel):
         channel: The channel to write the statistics to.
         """
 
-        logging.basicConfig(level = logging.INFO, format = '[%(levelname)s] %(asctime)s :: %(message)s')
+        logging.basicConfig(level = logging.INFO, format = "[%(levelname)s] %(asctime)s :: %(message)s")
 
-        model_id = "TheBloke/Mistral-7B-Instruct-v0.2-GGUF"
-        gguf_file = "mistral-7b-instruct-v0.2.Q4_K_M.gguf"
-
-        logging.info(f"Loading tokeniser and model {model_id}")
-        self._tokeniser = MistralTokenizer.v1()
-        self._model = AutoModelForCausalLM.from_pretrained(model_id, gguf_file = gguf_file)
-
-        logging.info("Preparing model using accelerator")
-        self._accelerator = Accelerator()
-        self._model, self._tokeniser = self._accelerator.prepare(self._model, self._tokeniser)
-        logging.info("Model prepared on {self._accelerator.device}")
-
-        # logging.info("Moving model to MPS")
-        # self._model.to("mps")
-
-        self._model: MistralForCausalLM
+        logging.info(f"Loading Llama model")
+        self._model = Llama.from_pretrained(
+            repo_id = "TheBloke/Mistral-7B-Instruct-v0.2-GGUF",
+            filename = "*.Q4_K_M.gguf",
+            verbose = False,
+        )
+        self._model : Llama
 
         self._measurements = measurements
         self._channel = channel
 
     @staticmethod
-    def _construct_messages(prompt: str) -> ChatCompletionRequest:
-        return ChatCompletionRequest(messages = [
-            SystemMessage(content = (
-                    'You always continue sentences provided by the user and '
-                    'you never repeat what the user already said.'
-                )),
-            UserMessage(content =
-                'Question: Is Jake a turtle?\nAnswer: Jake is ',
-            ),
-            AssistantMessage(content = 'not a turtle.'),
-            UserMessage(content = (
-                    'Question: What is Priya doing right now?\n'
-                    'Answer: Priya is currently '
-            )),
-            AssistantMessage(content = 'sleeping.'),
-            UserMessage(content = prompt),
-        ])
+    def _construct_messages(prompt: str) -> list[dict[str, str]]:
+        return [
+            {
+                "role": "system",
+                "content": (
+                    "You always continue sentences provided by the user and "
+                    "you never repeat what the user already said."
+                )
+            },
+            {
+                "role": "user",
+                "content": "Question: Is Jake a turtle?\nAnswer: Jake is "
+            },
+            { "role": "assistant", "content": "not a turtle." },
+            {
+
+                "role": "user",
+                "content": (
+                    "Question: What is Priya doing right now?\n"
+                    "Answer: Priya is currently "
+                )
+            },
+            { "role": "assistant", "content": "sleeping." },
+            { "role": "user", "content": prompt }
+        ]
+
+    @staticmethod
+    def _remove_newlines(text: str) -> str:
+        return text.replace("\n", "\\n")
 
     @override
     def sample_text(
@@ -106,25 +102,19 @@ class LocalModel(language_model.LanguageModel):
         timeout: float = language_model.DEFAULT_TIMEOUT_SECONDS,
         seed: int | None = None,
     ) -> str:
-        # gemma2 does not support `tokens` + `max_new_tokens` > 8193.
-        # gemma2 interprets our `max_tokens`` as their `max_new_tokens`.
         max_tokens = min(max_tokens, _DEFAULT_MAX_TOKENS)
 
         messages = self._construct_messages(prompt)
-        logging.info(f"Sending prompt with {len(prompt)} characters, beginning with: '{prompt[:32]}'.")
+        logging.info(f"Sending prompt with {len(prompt)} characters, beginning with: '{LocalModel._remove_newlines(prompt[:32])}'.")
 
-        tokens = self._tokeniser.encode_chat_completion(messages).tokens
-        logging.info(f"Tokenised prompt with {len(tokens)} tokens.")
-        input_ids = torch.tensor([tokens], device = self._accelerator.device)
-        outputs = self._model.generate(
-            input_ids,
-            max_new_tokens=max_tokens,
-            do_sample=True,
-            temperature=temperature,
-            seed=seed,
-        )
-        result = self._tokeniser.decode(outputs[0].tolist())
-        logging.info(f"Generated response with {len(result)} characters, beginning with: '{result[:32]}'.")
+        result = self._model.create_chat_completion(
+            messages,
+            temperature = temperature,
+            seed = seed,
+            max_tokens = max_tokens,
+        )["choices"][0]["message"]["content"]
+
+        logging.info(f"Generated response with {len(result)} characters, beginning with: '{LocalModel._remove_newlines(result[:32])}'.")
 
         if self._measurements is not None:
             self._measurements.publish_datum(
@@ -145,34 +135,47 @@ class LocalModel(language_model.LanguageModel):
         *,
         seed: int | None = None,
     ) -> tuple[int, str, dict[str, float]]:
+        prompt = dedent(f"""
+            {prompt}
 
-        def _score_response(response: str) -> float:
-            augmented_prompt = prompt + response
-            messages = self._construct_messages(augmented_prompt)
+            Pick one best response:
+            {" ".join(f"({i}) {response}" for i, response in enumerate(responses))}
+        """)
 
-            tokens = self._tokeniser.encode_chat_completion(messages).tokens
-            logging.info(f"Testing response {response} with prompt of {len(tokens)} tokens.")
+        for attempts in range(_MAX_MULTIPLE_CHOICE_ATTEMPTS):
+            # Increase temperature after the first failed attempt.
+            temperature = dynamically_adjust_temperature(attempts, _MAX_MULTIPLE_CHOICE_ATTEMPTS)
 
-            with torch.no_grad():
-                outputs = self._model(input_ids = tokens, labels = tokens)
+            messages = self._construct_messages(prompt)
+            logging.info(f"Sending prompt with {len(prompt)} characters, beginning with: '{LocalModel._remove_newlines(prompt[:32])}'.")
 
-                loss = outputs.loss
-                score = -loss.item()
+            result = self._model.create_chat_completion(
+                messages,
+                temperature = temperature,
+                seed = seed,
+                max_tokens = _DEFAULT_MAX_TOKENS,
+            )["choices"][0]["message"]["content"]
 
-            logging.info(f"Scored response {response} with score {score}.")
-            return score
+            answer = extract_choice_response(result)
 
-        logging.info(f"Scoring {len(responses)} responses.")
-        scores = np.array([_score_response(r) for r in responses])
+            try:
+                idx = int(answer)
+            except (TypeError, ValueError):
+                continue
+            else:
+                if self._measurements is not None:
+                    self._measurements.publish_datum(
+                        self._channel, {'choices_calls': attempts}
+                    )
 
-        # Find the index of the highest score
-        idx = np.argmax(scores)
-        max_str = responses[idx]
-        logging.info(f"Chose response {max_str} with score {scores[idx]}.")
+                return idx, responses[idx], {}
 
-        return idx, max_str, {r: scores[i] for i, r in enumerate(responses)}
+        raise language_model.InvalidResponseError(
+            (f'Too many multiple choice attempts.\nLast attempt: {result}, ' +
+             f'extracted: {answer}')
+        )
 
 if __name__ == "__main__":
     model = LocalModel()
     print(model.sample_text("Hello, how are you?"))
-    print(model.sample_choice("What is the capital of France", ["London", "Paris", "Your Mom"]))
+    print(model.sample_choice("What is the biggest city in the UK", ["London", "Paris", "Your Mom"]))
